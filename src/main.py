e8cf71e9e7a2e5c246dd138144950923a34c3022
dwseoh <dongwanseoh@gmail.com>
2024-08-25 22:32:56 -0400

    /

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
 import os
 import discord
 from discord.ext import commands
+
+
 from dotenv import load_dotenv  # This is the tool to load environment variables
 print("dotenv loaded successfully!")
 
 # Load environment variables from a .env file
 load_dotenv()
+token = os.getenv('DISCORD_BOT_TOKEN')
 
 
+intents = discord.Intents.all()
+activity = discord.Game(name="hello testing")
+
+client = commands.Bot(case_insensitive=True,
+                          command_prefix='?',
+                          intents=intents,
+                          status=discord.Status.dnd)
+
+@client.event
+async def on_ready():
+    print("We have logged in as", client.user)
+
+
+
+@client.command()
+async def test(ctx):
+    print("test")
+
+client.run(token)

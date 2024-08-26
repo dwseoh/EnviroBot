45bbb9e75997096ac26fc8cc7a5cff3e13f36db5
Renee <renee.xu321@gmail.com>
2024-08-25 21:52:09 -0400

    setup environment

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv  # This is the tool to load environment variables
+print("dotenv loaded successfully!")
+
+# Load environment variables from a .env file
+load_dotenv()
+
+

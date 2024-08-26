3fb40f44dbaec4eeeae456b5e22fc24b555f597b
Renee <renee.xu321@gmail.com>
2024-08-25 22:12:32 -0400

    REVERSE

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv  # This is the tool to load environment variables
+
+
 # Load environment variables from a .env file
 
 

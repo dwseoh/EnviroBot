0ad912413a5ac44138720c8f5f53dd6b96afe45f
Renee <renee.xu321@gmail.com>
2024-08-25 22:09:23 -0400

    ok

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv  # This is the tool to load environment variables
-
-TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-
 # Load environment variables from a .env file
+
+
+# HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOo testing
 load_dotenv()
 
 
